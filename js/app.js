15d760d07363626203c15aa84fb380a07a59cb0f
AweysP <aweyspemba@gmail.com>
2022-03-09 11:49:40 -0500

    backend done ish

---
+++ js/app.js
@@ -0,0 +1,75 @@
+import {
+    ClickCounter
+} from "./ClickCounter.js";
+
+let autoClick =0 ;
+let multiplierClick = 0;
+let multiplier = 1.2;
+let power = 0;
+let autoClickerCost = 50;
+let multiplierCost = 75;
+let priceMultiplier = .10;
+let donutCount = Math.pow(multiplier,power)
+let value = 0;
+
+let autoClickerCostTotal = autoClickerCost%priceMultiplier;
+
+
+const clickerEl = document.querySelector('.clicker');
+const autoClickerEl=document.querySelector('#purchaseAutoClicker');
+const multiplierEl = document.querySelector('#purchaseMultipliers');
+
+
+clickerEl.addEventListener('click', ()=>{
+    donutCount += Math.pow(multiplier,power)
+    updateDash();
+})
+
+autoClickerEl.addEventListener('click',()=>{
+    if(donutCount >= autoClickerCost){
+        donutCount = donutCount-autoClickerCost;
+        autoClick = autoClick +1;
+        autoClickerCost += Math.floor((autoClickerCost * 0.10));
+        updateDash();
+    }
+})
+multiplierEl.addEventListener('click', () =>{
+    if(donutCount >= multiplierCost){
+        donutCount = donutCount - multiplierCost;
+        multiplierClick = multiplierClick+1;
+        multiplierCost += Math.round(multiplierCost*priceMultiplier);
+        power++;
+        donutCount+=value;
+        priceMultiplier+=.10;
+        updateDash()
+    }
+})
+function timer() {
+    donutCount = donutCount + autoClick;
+    donutCount = donutCount + multiplierClick;
+    updateDash()
+};
+setInterval(timer, 1000)
+
+
+function updateDash() {
+
+    document.querySelector('.donut').value = Math.floor(donutCount)
+    document.querySelector('.donut').innerHTML = Math.floor(donutCount);
+    
+    document.title = Math.floor(donutCount) + "  Donuts";
+
+    document.querySelector('.numberOfAutoClicker').innerHTML = "You Have " + autoClick + " Auto Clickers.";
+
+    document.querySelector('.costOfAutoClicker').innerHTML = "Cost: " + autoClickerCost + " Donuts";
+
+    document.querySelector('.priceMultiplier').innerHTML = "Cost: " + multiplierCost + " Donuts";
+
+    document.querySelector('.numberOfMultipliers').innerHTML = "You Have " + multiplierClick + " Multipliers.";
+   
+
+    document.querySelector("#purchaseAutoClicker").innerHTML = (donutCount < 100) ? "Buy Auto Clicker" : "Buy Auto Clicker";
+    document.querySelector("#purchaseMultipliers").innerHTML = (donutCount < 25)? "Buy Auto Multiplier" : "Buy Auto Multiplier";
+
+    document.querySelector('.donutsPerSecond').innerHTML = Math.round(((autoClick+multiplierClick) * multiplier)) + " donuts per second";
+}
